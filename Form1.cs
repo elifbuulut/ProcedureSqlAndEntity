c73d4635f914338e097d313d9feb77285dee5fd5
elifb <elifb@DESKTOP-R9ITCEQ>
2022-03-28 02:12:55 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using ProcedureSqlAndEntity.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcedureSqlAndEntity
+{
+    public partial class Form1 : Form
+    {
+        DbOkulEntities db = new DbOkulEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            //Öğrenci Adı, Soyadı, Kulubu nun listelendiği procedure 
+            //------------------------
+            //SQLDE PROCEDURE OLUŞTUR
+            /*create procedure KULUPLER
+             * as
+             * SELECT Ogr_Ad as Öğrenci_Adı,Ogr_Soyad as Öğrenci_Soyad,KulupAd as Kulup_Adı FROM Tbl_Ogr
+             * inner join Tbl_OgrKulupleri on Tbl_OgrKulupleri.KulupId=Tbl_Ogr.KulupId
+             */
+            //-------------------------
+            dataGridView1.DataSource = db.KULUPLER();
+
+        }
+    }
+}
